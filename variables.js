6deee2ebc6453c2c279d91e189369137fba5e8c4
Trey Marshall <marshall.trey92@yahoo.com>
2022-11-08 17:00:44 -0600

    Fix syntax errors and order of operations

--- variables.js
+++ variables.js
@@ -1,8 +1,16 @@
-var firstName = "Trey"
-var lastName = "Marshall"
-var age = "30"
-var date = "11082022"
-var isWorking = "true"
-var isScared = "false"
-console.log;(firstName) = "Kelly"
-console.log;(lastName) = "B"
+var firstName = "Trey";
+var lastName = "Marshall";
+var age = "30";
+var date = "11082022";
+var isWorking = "true";
+var isScared = "false";
+
+firstName = "Kelly"
+lastName = "B"
+
+console.log(firstName); 
+console.log(lastName);
+console.log(age);
+console.log(date);
+console.log(isWorking);
+console.log(isScared);
